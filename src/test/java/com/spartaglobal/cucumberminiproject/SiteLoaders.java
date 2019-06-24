e1be187cc082af1033919ee299a16227ec4c34af
Your Name <you@example.com>
2019-06-24 17:00:24 +0100

    AccountPage POM added

--- src/test/java/com/spartaglobal/cucumberminiproject/SiteLoaders.java
+++ src/test/java/com/spartaglobal/cucumberminiproject/SiteLoaders.java
@@ -1,19 +1,26 @@
 package com.spartaglobal.cucumberminiproject;
 
+import com.spartaglobal.cucumberminiproject.sitepages.AccountPage;
 import com.spartaglobal.cucumberminiproject.sitepages.HomePage;
 import org.openqa.selenium.WebDriver;
 
 public class SiteLoaders {
     private WebDriver driver;
     HomePage homePage;
+    AccountPage accountPage;
 
     public SiteLoaders(WebDriver driver){
         this.driver = driver;
         homePage = new HomePage(driver);
+        accountPage = new AccountPage(driver);
     }
 
     //Pages
     public HomePage getHomePage() {
         return homePage;
+    }
+
+    public AccountPage getAccountPage() {
+        return accountPage;
     }
 }
